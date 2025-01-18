f7844bb9dca8d3f0bdc3c3c90ba4e0651273a00a
SeemaKhanam <148565233+SeemaKhanam@users.noreply.github.com>
2025-01-18 10:58:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ with st.sidebar:
   data={'island',island,
         'bill_length_mm',bill_length_mm,
         'bill_depth_mm',bill_depth_mm,
-        'flipper_length_mm',flipper_length,
+        'flipper_length_mm',flipper_length_mm,
         'body_mass_g',body_mass_g,
         'gender',gender}
   input_df=pd.DataFrame(data,index[0])
