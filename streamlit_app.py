a43d073e109f4d7bfe2346e34e12e7a70d754c39
SeemaKhanam <148565233+SeemaKhanam@users.noreply.github.com>
2025-01-18 14:16:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,27 @@ with st.sidebar:
         'gender':gender}
   input_df=pd.DataFrame(data,index=[0])
   input_penguins=pd.concat([input_df,X],axis=0)
-input_df
+# Preprocessing 
+#encode x
+encode=['island','sex']
+df_penguine=pd.get_dummies(input_penguins,prefix=encode)
+x=df_penguins[1:]
+input_row=df_penguins[:1]
 
+#Encode y
+target_mapper={'Adelie':0,
+               'Chinstrap':1,
+               'Gentoo':2}
+def target_encode(val):
+  return target_mapper[val]
+
+y=y_raw.apply(target_encode)
+
+with st.expander('Data preparation'):
+  st.write('**Encoded X (input penguin)**')
+  input_row
+  st.write('**Encoded y**')
+  y
+  
         
         
