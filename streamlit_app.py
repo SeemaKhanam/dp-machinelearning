c05c2340bcd5942b626361c085952e59001bb20e
SeemaKhanam <148565233+SeemaKhanam@users.noreply.github.com>
2025-01-18 10:47:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,8 @@ with st.expander('Data Visulization'):
 # Data Preparation
 with st.sidebar:
   st.header('Input Features')
+  island=st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+  gender=st.selectbox('Gender','('male','female'))
+  bill_length_mm=st.slider('Bill length (mm)',32.1,59.6,43.9) #(min,max,avg)
+  
   
