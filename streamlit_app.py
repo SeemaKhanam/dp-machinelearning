ca7bcb5c4d1663a15c8cef3369e7e35a9f594852
SeemaKhanam <148565233+SeemaKhanam@users.noreply.github.com>
2025-01-18 10:32:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,11 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df=pd.read_csv("https://raw.githubusercontent.com/psykick-21/penguin-species-prediction-streamlit/ea896a194e70140227e638724c7af21349bedf99/datasets/penguins_cleaned.csv")
   df
-  
+
+  st.write('**X**')
+  X=df.iloc[:,1:]
+  X
+
+  st.write('**y**')
+  y=df.iloc[:,0]
+  y
