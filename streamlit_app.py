d0102d8b534dd8d8483200678cb9a4cd243d5a79
SeemaKhanam <148565233+SeemaKhanam@users.noreply.github.com>
2025-01-18 10:27:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🎈 Machine Learning App')
 
 st.info('This app builds a machine learning model')
+df=pd.read_csv("https://raw.githubusercontent.com/psykick-21/penguin-species-prediction-streamlit/ea896a194e70140227e638724c7af21349bedf99/datasets/penguins_cleaned.csv")
+df
